c0caaba6a76edb9e0aa6dae5c30eb475afa853b0
amitcoders <amitdigital56@gmail.com>
2024-10-05 17:39:50 +0530

    add

--- resources/views/layouts/home/navbar.blade.php
+++ resources/views/layouts/home/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
     <div class="container">
-        <a class="navbar-brand" href="index.html">Car<span>Book</span></a>
+        <a class="navbar-brand" href="">Car<span>Book</span></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
             aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="oi oi-menu"></span> Menu
@@ -8,7 +8,7 @@
 
         <div class="collapse navbar-collapse" id="ftco-nav">
             <ul class="navbar-nav ml-auto">
-                {{-- <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li> --}}
+                {{-- <li class="nav-item active"><a href="" class="nav-link">Home</a></li> --}}
                 <li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
                 <li class="nav-item"><a href="about.html" class="nav-link">Cars</a></li>
             </ul>
